03085dac3ff69c954ed4fcb6f68e1f1eae059780
AhaD <abuahad35188@gmail.com>
2023-06-08 19:56:34 +0600

    axios instal and creat usehook and also usees useclass hook

--- src/hooks/useClass.jsx
+++ src/hooks/useClass.jsx
@@ -1,13 +1,17 @@
 import { useQuery } from "@tanstack/react-query"
 import useAuth from "./useAuth"
+import useAxiosSecure from "./useAxiosSecure";
 
 const useClass = ()=>{
-     const {user} = useAuth();
+     const {user,loading} = useAuth();
+     const [axiosSecure] = useAxiosSecure();
+     
      const {refetch, data: classes = []} = useQuery({
           queryKey:['my-class',user?.email],
+          enabled: !loading,
           queryFn : async()=>{
-               const res = await fetch(`http://localhost:5000/my-class?email=${user?.email}`)
-               return res.json()
+               const res = await axiosSecure(`/my-class?email=${user?.email}`)
+               return res.data
           }
      })
      return [classes,refetch]
